881fedec6fb5c62f5c5f44fd56ff50a9e177fdca
Shira Boodaei <shiraboodaei@Shiras-MacBook-Air.local>
2024-11-07 19:17:45 +0200

    Divide only the rate value with 100

--- FVCalc.java
+++ FVCalc.java
@@ -2,11 +2,11 @@
 public class FVCalc {
 	public static void main(String[] args){
 		double currValue = Double.parseDouble(args[0]);
-		double interestRate = Double.parseDouble(args[1]);
-		double numOfYears = Double.parseDouble(args[2]);
+		double interestRate = Double.parseDouble(args[1])/100;
+		int numOfYears = Integer.parseInt(args[2]);
 		//Calc future value
-		double futureValue = (currValue * (Math.pow((1 + interestRate),numOfYears)))/100;
-		System.out.println("After " + (int) numOfYears + " years, $" +
+		double futureValue = currValue * Math.pow(1 + interestRate,numOfYears);
+		System.out.println("After " + (int) numOfYears + " years, a $" +
 							(int) currValue + " saved at " + interestRate +
 							"% will yield $" + (int) futureValue);
 	}
