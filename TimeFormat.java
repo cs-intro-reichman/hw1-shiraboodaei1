b64493ab8a1fc89b87c216aae1a4a816f4e5fef4
Shira Boodaei <shiraboodaei@Shiras-MacBook-Air.local>
2024-11-07 21:45:49 +0200

    TimeFormat class

--- TimeFormat.java
+++ TimeFormat.java
@@ -12,6 +12,18 @@ public class TimeFormat {
 		int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
 		// Does the same with the minutes part of the input.
 		int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        // Replace this comment with the rest of your code
+		// Get new hour format 
+		if (hours > 12)
+			hours = hours - 12;
+		System.out.print(hours + ":");
+		// Get minutes format 
+		if (minutes < 10)
+			System.out.print(0); 
+		System.out.print(minutes + " ");
+		// Define AM/PM
+		String AmPm = "AM";
+		if (hours >= 12)
+			AmPm = "PM";
+		System.out.print(AmPm);	
 	}
 }
